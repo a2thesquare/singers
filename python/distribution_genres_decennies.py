87c216f0bb09550545eba8c6259e5fd8842fa6c1
a2thesquare <160767407+a2thesquare@users.noreply.github.com>
2025-08-27 16:15:07 +0200

    Create distribution_genres_decennies.py

---
+++ python/distribution_genres_decennies.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("combined_singers.csv") # lecture du csv
+
+df['birth_date'] = pd.to_datetime(df['birthDate'], errors='coerce') # conversion en datatime pour pouvoir le traiter comme une date
+
+df['birth_year'] = df['birth_date'].dt.year # extraction de l'année
+
+df['decade'] = (df['birth_year'] // 10) * 10 # calcul de la decennie
+
+df = df[df['genderLabel'].isin(['male', 'female'])]
+
+decade_gender_counts = df.groupby(['decade', 'genderLabel'])['singerLabel'].count().unstack(fill_value=0)
+
+decade_gender_counts.plot(kind='bar', figsize=(12,6), color=['lightpink', 'skyblue'])
+
+plt.xlabel('Decade of Birth')
+plt.ylabel('Number of Singers')
+plt.title('Number of Male and Female Singers by Decade')
+plt.xticks(rotation=45)
+plt.legend(title='Gender')
+plt.tight_layout()
+plt.show()
