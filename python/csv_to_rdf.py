ad121b80172803cbe4945235ddedc72f7aee0898
a2thesquare <160767407+a2thesquare@users.noreply.github.com>
2025-08-29 18:03:39 +0200

    Add files via upload

---
+++ python/csv_to_rdf.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from rdflib import Graph, URIRef, Literal, Namespace
+
+df = pd.read_csv("combined_singers.csv")  # ton CSV exporté
+g = Graph()
+EX = Namespace("http://example.org/")  # ton namespace
+
+for idx, row in df.iterrows():
+    sujet = URIRef(f"http://example.org/ligne{idx}")
+    for col in df.columns:
+        g.add((sujet, EX[col], Literal(row[col])))
+
+g.serialize("wikidata.rdf", format="turtle")
