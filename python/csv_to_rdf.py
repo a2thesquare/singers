a00669cd66b8106ebf0f05d4c1e3112962a4138f
a2thesquare <160767407+a2thesquare@users.noreply.github.com>
2025-08-31 20:50:07 +0200

    Update csv_to_rdf.py

--- python/csv_to_rdf.py
+++ python/csv_to_rdf.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from rdflib import Graph, URIRef, Literal, Namespace
 
-df = pd.read_csv("combined_singers.csv")  # ton CSV exporté
+df = pd.read_csv("combined_singers.csv")
 g = Graph()
-EX = Namespace("http://example.org/")  # ton namespace
+EX = Namespace("http://example.org/")
 
 for idx, row in df.iterrows():
     sujet = URIRef(f"http://example.org/ligne{idx}")
